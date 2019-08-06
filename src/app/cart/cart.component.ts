dc798f711b1686a991d19ef701ce378301ca839f
PramodKalabhavi <pramodkumar93007@gmail.com>
2019-08-06 16:54:21 +0530

    3 files modified, 2 added

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -7,7 +8,8 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit {
 items;
-  constructor(private cartService: CartService) {
+  constructor(private cartService: CartService,
+  private http :HttpClient,) {
      this.items = this.cartService.getItems();
    }
 
